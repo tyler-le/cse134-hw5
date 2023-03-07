a2bd2e4fa9e18f8ee5f7d0cd67dca7892a7c6a50
Tyler Le <tylerle99@yahoo.com>
2023-03-06 16:24:42 -0800

    fixed GET and delete

--- source/methodtest.js
+++ source/methodtest.js
@@ -15,17 +15,27 @@ export function doPost() {
         .catch((error) => console.error(error));
 }
 
+// TODO: Need to pass in data to GET request
 export function doGet() {
     const response = document.getElementById("response");
+    const id = document.getElementById("id").value;
+    const articleName = document.getElementById("article_name").value;
+    const articleBody = document.getElementById("article_body").value;
+    const date = document.getElementById("date").value;
 
-    fetch("https://httpbin.org/get")
+    const url = new URL("https://httpbin.org/get");
+    url.searchParams.append("id", id);
+    url.searchParams.append("article_name", articleName);
+    url.searchParams.append("article_body", articleBody);
+    url.searchParams.append("date", date);
+
+    fetch(url)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
             let formattedData = JSON.stringify(data, null, 2);
             response.innerHTML = `<pre>${formattedData}</pre>`;
         })
-
         .catch((error) => console.error(error));
 }
 
@@ -48,8 +58,12 @@ export function doPut() {
 
 export function doDelete() {
     const response = document.getElementById("response");
+    const id = document.getElementById("id").value;
 
-    fetch("https://httpbin.org/delete", {
+    const url = new URL("https://httpbin.org/delete");
+    url.searchParams.append("id", id);
+
+    fetch(url, {
         method: "DELETE",
     })
         .then((response) => response.json())
